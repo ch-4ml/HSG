0b82ae1efc36133e1ce14ac9656d798f2b74145e
Yu <Yu@DESKTOP-DLUELJP>
2019-06-13 21:24:30 +0900

    delete, update 추가

--- src/main/java/com/hsg/intro/opn/lat/controller/OpnLatController.java
+++ src/main/java/com/hsg/intro/opn/lat/controller/OpnLatController.java
@@ -90,62 +90,183 @@ public class OpnLatController {
 		return mv;
 	}
 	
+	// contents 추가
 	@RequestMapping(value = "insert.lt", method = RequestMethod.POST) // DI 의존성 주입 
 	public ModelAndView insertOpnLat(Contents c, ModelAndView mv,
-	  @RequestParam(required=false) MultipartFile file, HttpServletRequest request) {
+			@RequestParam(required=false) MultipartFile file, HttpServletRequest request) {
 	  
-		  // ################### 파일 업로드 ################### 
-		  String root = request.getSession().getServletContext().getRealPath("resources"); 
-		  String filePath = root + "/uploadFiles/opnlat_upload_file"; 
-		  String fileName = "";
-		  String originFileName = "";
-		  try { // 파일명 새이름 설정 
-			  int randomNumber = (int)((Math.random()*10000)+1);
-			  SimpleDateFormat format = new SimpleDateFormat ( "yyyyMMddHHmmss"); 
-			  Date nowTime = new Date(); String newfileName = format.format(nowTime) + String.valueOf(randomNumber);
+		// ################### 파일 업로드 ################### 
+		String root = request.getSession().getServletContext().getRealPath("resources"); 
+		String filePath = root + "/uploadFiles/opnlat_upload_file"; 
+		String fileName = "";
+		String originFileName = "";
+		
+		try { // 파일명 새이름 설정 
+			int randomNumber = (int)((Math.random()*10000)+1);
+			SimpleDateFormat format = new SimpleDateFormat ( "yyyyMMddHHmmss"); 
+			Date nowTime = new Date(); String newfileName = format.format(nowTime) + String.valueOf(randomNumber);
 			  
-			  // 확장자 구하기 
-			  int pos = file.getOriginalFilename().lastIndexOf("."); 
-			  String ext = file.getOriginalFilename().substring(pos); 
-			  fileName = newfileName + ext;
-			  originFileName = file.getOriginalFilename();
+			// 확장자 구하기 
+			int pos = file.getOriginalFilename().lastIndexOf("."); 
+			String ext = file.getOriginalFilename().substring(pos); 
+			fileName = newfileName + ext;
+			originFileName = file.getOriginalFilename();
 			  
-			  c.setContents(fileName);
-			  c.setOrigin(originFileName);
+			c.setContents(fileName);
+			c.setOrigin(originFileName);
 			  
-			  // 폴더 없으면 생성 
-			  File uploadPath = new File(filePath); 
-			  if(!uploadPath.exists()) {
-				  uploadPath.mkdirs(); 
-			  }
-			  
-			  // 파일경로를 itrbok 객체에 넣어줌 
-			  filePath = filePath + "/" + fileName;
-			  
-			  // 해당 폴더에 파일 생성 
-			  file.transferTo(new File(filePath));
-			  
-		  } catch (IllegalStateException e1) { 
-			  e1.printStackTrace(); 
-		  } catch (IOException e1) { 
-			  e1.printStackTrace(); 
-		  } 
-		  // ################### 파일 업로드###################
+			// 폴더 없으면 생성 
+			File uploadPath = new File(filePath); 
+		if(!uploadPath.exists()) {
+			uploadPath.mkdirs(); 
+		}
 		  
-		  c.setPageId(pageId); c.setPostDate(postDate);
+		// 파일경로를 itrbok 객체에 넣어줌 
+		filePath = filePath + "/" + fileName;
 		  
-		  try { 
-			  csi.insert(c); 
-			  mv.setViewName("redirect:view.lt");
+		// 해당 폴더에 파일 생성 
+		file.transferTo(new File(filePath));
+			  
+		} catch (IllegalStateException e1) { 
+			e1.printStackTrace(); 
+		} catch (IOException e1) { 
+			e1.printStackTrace(); 
+		} 
+		// ################### 파일 업로드###################
 		  
-		  } catch (Exception e) { mv.addObject("message",e.getMessage());
-		  	  mv.setViewName("common/errorPage");
+		c.setPageId(pageId); c.setPostDate(postDate);
 		  
-		  } 
-		  return mv;
-	  
-	  }
+		try { 
+			csi.insert(c); 
+			mv.setViewName("redirect:view.lt");
+		  
+		} catch (Exception e) { mv.addObject("message",e.getMessage());
+			mv.setViewName("common/errorPage");
+				  
+		} 
+		
+		return mv;  
+	}
 	
-	  
-	 
+	// contents 수정
+	@RequestMapping(value = "update.lt", method = RequestMethod.POST) // DI 의존성 주입
+	public ModelAndView updateOpnLat(Contents c, ModelAndView mv, 
+			@RequestParam(required=false) MultipartFile file, 
+			HttpServletRequest request) throws ContentsException {
+		try {
+				System.out.println("#################### update.lt file.isEmpty() : " + file.isEmpty() + "####################");
+				System.out.println("#################### update.lt content : " + c + "####################");
+				
+				if(!file.isEmpty()) { // 파일이 null 일 경우
+					String root = request.getSession().getServletContext().getRealPath("resources");
+					String filePath = root + "/uploadFiles/opnlat_upload_file";
+					String fileName = "";
+					String updatefilePath = "";
+					String originFileName = "";
+					
+					// ##################### 파일 삭제 처리 #######################
+					String deleteFileName = csi.findById(c.getId()).getContents();
+
+					// 파일명 새이름 설정
+					int randomNumber = (int)((Math.random()*10000)+1);
+					SimpleDateFormat format = new SimpleDateFormat ("yyyyMMddHHmmss");
+					Date nowTime = new Date();
+					String newfileName = format.format(nowTime) + String.valueOf(randomNumber);	
+					
+					// 확장자 구하기
+					int pos = file.getOriginalFilename().lastIndexOf(".");
+					String ext = file.getOriginalFilename().substring(pos);
+					fileName = newfileName + ext;
+					originFileName = file.getOriginalFilename();
+					
+					//파일경로를 contents 객체에 넣어줌
+					c.setContents(fileName);
+					c.setOrigin(originFileName);
+					
+					System.out.println("#################### update.lt content : " + c + "####################");
+					updatefilePath = filePath + "/" + fileName;
+					System.out.println("#################### update.lt updatefilePath : " + updatefilePath + "####################");
+					
+					// 해당 폴더에 파일 생성
+					file.transferTo(new File(updatefilePath));
+					
+					// ##################### 파일 삭제 처리 #######################
+					String deleteFilePath = filePath + "/" + deleteFileName;
+					
+					// ##################### 파일 삭제 처리 #######################
+					File deleteFile = new File(deleteFilePath); // 파일 URL
+					
+					System.out.println("#################### update.lt deleteFilePath : " + deleteFilePath + "####################");
+					
+					if(deleteFile.exists()) {
+						if(deleteFile.delete()) {
+							System.out.println("파일 삭제 완료");
+						} else {
+							System.out.println("파일 삭제 실패");
+						}
+					} else {
+						System.out.println("파일이 존재하지 않습니다.");
+					}
+				}
+				
+		} catch (IllegalStateException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		c.setPageId(pageId);
+		c.setPostDate(postDate);
+		
+		try {
+			csi.update(c);
+			
+			List<Contents> cs = csi.findByPageId(pageId);
+			mv.addObject("cs", cs);
+			mv.setViewName("redirect:view.lt");
+		} catch (Exception e) {
+			mv.addObject("message",e.getMessage());
+			mv.setViewName("redirect:/common/errorPage");
+
+		}
+		return mv;
+	}
+	
+	// contents 삭제 
+	@RequestMapping(value = "delete.lt", method = RequestMethod.GET) // DI 의존성 주입
+	public ModelAndView deleteOpnLat(
+			ModelAndView mv,
+			@RequestParam(value="id") int id,
+			HttpServletRequest request){
+		
+		try {
+			String root = request.getSession().getServletContext().getRealPath("resources");
+			String filePath = root + "/uploadFiles/opnlat_upload_file";
+			String deleteFileName = csi.findById(id).getContents();
+			// ##################### 파일 삭제 처리 #######################
+			String deleteFilePath = filePath + "/" + deleteFileName;
+			
+			// ##################### 파일 삭제 처리 #######################
+			File deleteFile = new File(deleteFilePath); // 파일 URL
+			
+			System.out.println("#################### update.ib deleteFilePath : " + deleteFilePath + "####################");
+			
+			if(deleteFile.exists()) {
+				if(deleteFile.delete()) {
+					System.out.println("파일 삭제 완료");
+				} else {
+					System.out.println("파일 삭제 실패");
+				}
+			} else {
+				System.out.println("파일이 존재하지 않습니다.");
+			}
+			
+			csi.delete(id);
+			mv.setViewName("redirect:view.ib");
+		} catch (ContentsException e) {
+			mv.addObject("message",e.getMessage());
+			mv.setViewName("redirect:/common/errorPage");
+		}
+		return mv;
+	}
 }
